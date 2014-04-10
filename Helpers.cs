040c675c22567798801c172af91115dcf1e57d25
wip <wip-@users.noreply.github.com>
2014-04-10 18:20:26 +0900

    Add utility function CreateJaggedArray()

--- Helpers.cs
+++ Helpers.cs
@@ -306,6 +306,28 @@ namespace WilCommon
 
 
 
+        // http://stackoverflow.com/a/1739058/758666
+        public static T CreateJaggedArray<T>(params int[] lengths)
+        {
+            return (T)InitializeJaggedArray(typeof(T).GetElementType(), 0, lengths);
+        }
+        static object InitializeJaggedArray(Type type, int index, int[] lengths)
+        {
+            Array array = Array.CreateInstance(type, lengths[index]);
+            Type elementType = type.GetElementType();
+
+            if (elementType != null)
+            {
+                for (int i = 0; i < lengths[index]; i++)
+                {
+                    array.SetValue(
+                        InitializeJaggedArray(elementType, index + 1, lengths), i);
+                }
+            }
+
+            return array;
+        }
+
 
 
 
